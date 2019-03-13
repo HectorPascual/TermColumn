37461376b89000a085e97d15ebda945be18eee01
Hector <rcpascualhector@gmail.com>
2019-03-13 19:40:40 +0100

    added readme

--- TermColumn.java
+++ TermColumn.java
@@ -3,11 +3,11 @@ import java.util.*;
 
 class TermColumn {
   public static void main(String[] args) {
-      if(args.length!=0) paintTermColumns(getTermColumns(),args[0]);
+      if(args.length!=0) paintTermColumns(getTermColumns(),args[0]); // can be executed in mode random and mode normal
       else paintTermColumns(getTermColumns(),"normal");
   }
 
-  public static int getTermColumns(){
+  public static int getTermColumns(){ // return number of columns || TO-DO: achieve same result with tput
     //String[] cmd = {"/bin/bash", "-c", "tput cols 2< /dev/tty"};
     String[] cmd = {"/bin/bash", "-c", "stty size < /dev/tty | cut -d ' ' -f 2"};
     int cols = 0;
@@ -20,14 +20,14 @@ class TermColumn {
     return cols;
   }
 
-  public static void paintTermColumns(int columns, String mode){
+  public static void paintTermColumns(int columns, String mode){ // paint each column
     Random rgen = new Random();
     for(int i = 0; i < columns; i++){
       if (mode.equals("random")) System.out.print("\033["+(40+rgen.nextInt(8))+"m");
       else System.out.print("\033[47m");
       System.out.print(" ");
       try{
-        Thread.sleep(5);
+        Thread.sleep(5); // painting speed
       }
       catch (InterruptedException ex) {
       }
---
+++ readme.md
@@ -0,0 +1,25 @@
+# TermColumn
+> Get terminal number of colums and also is able to paint them in two modes.
+
+
+## Usage
+
+Once the .class file is compiled
+
+```sh
+javac TermColumn.java
+```
+
+You can run the program in two ways
+
+Normal Mode : 
+
+```sh
+java TermColumn
+```
+
+Random Mode : 
+
+```sh
+java TermColumn random
+```
